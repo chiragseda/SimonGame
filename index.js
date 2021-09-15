6dc5fe5a51db256a6236039de3235a2c5535ade9
chiragseda <36998229+chiragseda@users.noreply.github.com>
2021-09-15 23:42:36 +0530

    some more features added

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 
 var buttonColours=["red", "blue", "green", "yellow"];
 var gamePattern=[];
+var level=0;
 var userClickedPattern=[];
+var started = false;
+
 $(".btn").click(function(event) {
     var userChosenColour= event.target.id
     userClickedPattern.push(userChosenColour);
     animatePress(userChosenColour);
     makeSound(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
+});
+
+$(document).keydown(function(event)
+{
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+      }
 });
 
 function nextSequence()
 {
+    level++;
+    $("#level-title").text("Level " + level);
+    $("h1").text("Level "+level);
     var randomNumber=Math.floor(Math.random() * 4);
     var randomChosenColor=buttonColours[randomNumber];
     gamePattern.push(randomChosenColor);
     $("#"+randomChosenColor).fadeOut(100).fadeIn(100);
     makeSound(randomChosenColor);
+  
+}
+
+function checkAnswer(currentLevel)
+{
+    alert(currentLevel);
 }
 
 function makeSound(randomChosenColor)
